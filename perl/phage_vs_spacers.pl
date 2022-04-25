114d7506237ee489ae4b6d9fc97b11ed08afb5a7
arubval <84905997+arubval@users.noreply.github.com>
2022-04-25 10:56:08 +0200

    Update phage_vs_spacers.pl

--- perl/phage_vs_spacers.pl
+++ perl/phage_vs_spacers.pl
@@ -33,9 +33,9 @@ foreach my $var (keys %hash){
 	$file=$var.".fasta";
 	my $out=$var.".out";
 	my $outfil=$var."_filtrado.out";
-	#open (OUT, ">/home/arubval/Proyecto_Ministerio/spacers_vs_db/phage_vs_spab/phage/$file");
-	#print OUT ">$var\n$hash{$var}\n";
-	#close OUT;
+	open (OUT, ">/home/arubval/Proyecto_Ministerio/spacers_vs_db/phage_vs_spab/phage/$file");
+	print OUT ">$var\n$hash{$var}\n";
+	close OUT;
 	`makeblastdb -in $file -dbtype nucl`;
 	`blastn -task blastn-short -query spacers_ab_filtrate.fasta -db $file -outfmt '6 qseqid sseqid pident qcovs qcovhsp length qlen slen evalue qstart qend sstart send' -max_target_seqs 100 -num_threads 40 | awk '\$3 >= 95 && \$4 >= 95' > $out`;
 	open (FILE, "/home/arubval/Proyecto_Ministerio/spacers_vs_db/phage_vs_spab/Resultados/$out");
